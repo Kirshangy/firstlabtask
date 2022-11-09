8b4fa99b808eb710c4a852a21f736be0608c924c
Kirshangy <117634170+Kirshangy@users.noreply.github.com>
2022-11-09 09:49:08 -0800

    Add files via upload

---
+++ labtast1.java
@@ -0,0 +1,15 @@
+ public class labtast1 {
+    public static void main(String[] args) {
+        int mySetNo = 37;               // integer (whole number)
+        float myNumber = 0342.832f;    // floating point number
+        char myLetter = 'K';         // character
+        boolean mybool = true;       // boolean
+        String myText = "My Name Is kirshan iam also  Student of --UNIVERSTY OF SINDH JAMSHORO-- DEPT OF: CS    ";     // String
+        System.out.println(mySetNo);
+        System.out.println(myNumber);
+        System.out.println(myLetter);
+        System.out.println(mybool);
+        System.out.println(myText);
+    }
+}
+
